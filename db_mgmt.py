2f56bc53117dd21231862180fe504e763855329d
raunakchowdhury <rchowdhury5@stuy.edu>
2018-10-17 16:35:47 -0400

    finished clear story

--- db_mgmt.py
+++ db_mgmt.py
@@ -14,16 +14,30 @@ def create_db():
     c.execute('CREATE TABLE stories (title TEXT PRIMARY KEY, story TEXT, last_edit TEXT)')
 
 
-def create_story(title, content, user):
+def create_story(title, content, last_edit):
     '''
     Takes the title, content, and user and adds it to the dbs
+    Assumes that the user has been verified by Flask.
     '''
     db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
     c = db.cursor()               #facilitate db ops
 
-    c.execute('SELECT {} FROM user'.format(user))
+    command_tuple = (title, content, last_edit)
+    c.execute('INSERT INTO stories VALUES(?,?,?)',command_tuple)
+    c.execute('SELECT * FROM stories')
 
-     
+    for entry in c:
+        print(entry)
+
+    db.close()
+
+def auth_user(user):
+    '''
+    ensures the user is in the db.
+    '''
+    pass
+
 
 if __name__ == '__main__':
-    #create_db()
+    create_db()
+    create_story("soojinchoi","soojinchoi","soojinchoi")
