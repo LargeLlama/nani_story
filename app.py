bef13ef39053e0e09dd6f0093e3ca8b8cdbc01c8
JoanChirinos <jchirinos@stuy.edu>
2018-10-19 13:35:34 -0400

    .

--- app.py
+++ app.py
@@ -3,3 +3,39 @@ import os
 app = Flask(__name__) #create instance of class Flask
 
 app.secret_key = os.urandom(32)
+
+@app.route('/')
+def root_redirect():
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    else:
+        return render_template('landing.html')
+
+@app.route('/login', methods=['POST'])
+def authenticate():
+    username = request.form['username']
+    password = request.form['password']
+    action = request.form['action']
+
+    if (action == 'Login'):
+
+        session['username'] = username
+
+        return render_template('home.html', username=session['username'])
+
+    elif (action == 'Create Account'):
+
+        #create Account
+
+        session['username'] = username
+
+        return render_template('home.html', username=session['username'])
+
+@app.route('/home')
+def home():
+    
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
